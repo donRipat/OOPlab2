e6d11c75a41603a8fec68aaa0d7a8a9a0ee1eca0
РР <rafikov864@yandex.ru>
2020-12-19 11:07:39 +0500

    Добавил комментарии

--- OOPlab2/OOPlab2.cpp
+++ OOPlab2/OOPlab2.cpp
@@ -1,80 +1,80 @@
 #include <iostream>
 
-class Point
+class Point	//	точка
 {
 protected:
 	int x;
 	int y;
 public:
-	Point() 
+	Point() //	к-р по умолчанию
 	{
 		printf("Point()\n");
 		x = 0;
 		y = 0;
 	}
-	Point(int x, int y)
+	Point(int x, int y)	//	к-р с параметрами
 	{
 		printf("Point(%i, %i)\n", x, y);
 		this->x = x;
 		this->y = y;
 	}
-	Point(Point &p)
+	Point(Point &p)	//	к-р копирования
 	{
 		printf("Point(Point &p)\n");
 		x = p.x;
 		y = p.y;
 	}
-	virtual ~Point()
+	virtual ~Point()	// д-р
 	{
 		printf("~Point(%d, %d)\n", x, y);
 	}
-	void move(int dx, int dy)
+	void move(int dx, int dy)	//	метод сдвига точки
 	{
 		printf("move(%d, %d)\n", dx, dy);
 		x += dx;
 		y += dy;
 	}
-	void reset();
+	void reset();	//	объявление метода обнуления точки
 };
 
-void Point::reset() 
+void Point::reset() //	описание метода обнуления вне класса
 {
 	printf("reset(%i, %i)\n", x,y);
 	x = 0;
 	y = 0;
 }
 
-class Point3D: public Point
+class Point3D: public Point	//	класс-наследник точки -- точка в пространстве
 {
 protected:
 	int z;
 public:
-	Point3D(): Point()
+	Point3D(): Point()	//	к-р по умолчанию
 	{
 		printf("Point3D()\n");
 		z = 0;
 	}
-	Point3D(int x, int y, int z): Point(x, y)
+	Point3D(int x, int y, int z): Point(x, y)	//	к-р с параметрами
 	{
 		printf("Point3D(%i, %i, %i)\n", x, y, z);
 		this->z = z;
 	}
-	Point3D(Point3D &p):Point(p.x,p.y)
+	Point3D(Point3D &p):Point(p.x,p.y)	//	к-р копирования
 	{
 		printf("Point3D(Point3D &p)\n");
 		z = p.z;
 	}
-	~Point3D()
+	~Point3D()	//	д-р
 	{
 		printf("~Point3D(%d, %d, %d)\n", x, y, z);
 	}
-	void move3D(int dx, int dy, int dz)
+	void move3D(int dx, int dy, int dz)	//	метод сдвига точки в пространстве
 	{
 		printf("move3D(%d, %d, %d)\n", dx, dy, dz);
 		move(dx, dy);
 		z += dz;
 	}
-	void reset3D() 
+	void reset3D() //	метод обнуления точки в пространстве
 	{
 		printf("reset3D(%d, %d, %d)\n", x, y, z);
 		reset();
@@ -82,37 +82,37 @@ public:
 	}
 };
 
-class Segment
+class Segment	//	отрезок -- композиция 
 {
 protected:
 	Point *p1;
 	Point *p2;
 public:
-	Segment()
+	Segment()	//	к-р по умолчанию
 	{
 		printf("Segment()\n");
 		p1 = new Point;
 		p2 = new Point;
 	}
-	Segment(int x1, int y1, int x2, int y2)
+	Segment(int x1, int y1, int x2, int y2)	//	к-р с параметрами
 	{
 		printf("Segment(%i, %i, %i, %i)\n", x1, y1, x2, y2);
 		p1 = new Point(x1, y1);
 		p2 = new Point(x2, y2);
 	}
-	Segment(Segment &s)
+	Segment(Segment &s)	//	к-р копирования
 	{
 		printf("Segment(Segment &s)\n");
 		p1 = new Point(*(s.p1));
 		p2 = new Point(*(s.p2));
 	}
-	~Segment()
+	~Segment()	//	д-р
 	{
 		printf("~Segment()\n");
 		delete p1;
 		delete p2;
 	}
-	void reset() 
+	void reset() //	метод обнуления отрезка
 	{
 		printf("reset()\n");
 		p1->reset();
